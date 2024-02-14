29b85a24d145d8c16ce1de34854febe55e4defd9
Ahmad Reza A <areza7095@gmail.com>
2024-02-15 01:33:25 +0700

    completing cars feature

--- controllers/carController.go
+++ controllers/carController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"net/http"
 	"rental-car/initializers"
 	"rental-car/models"
 
@@ -10,29 +11,175 @@ import (
 func PostCarV1(c *gin.Context) {
 	// Get data req body
 	var bodyCar struct {
-		CarId     int
-		CarName   string
-		DayRate   float64
-		MonthRate float64
-		Image     string
+		CarName   string  `json:"car_name" binding:"required"`
+		DayRate   float64 `json:"day_rate" binding:"required"`
+		MonthRate float64 `json:"month_rate" binding:"required"`
+		Image     string  `json:"image" binding:"required"`
 	}
 
-	c.Bind(&bodyCar)
+	if err := c.BindJSON(&bodyCar); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    http.StatusBadRequest,
+			"message": "Bad Request: Invalid JSON payload",
+		})
+		return
+	}
 
 	// Create a Post
-	car := models.Car{CarID: bodyCar.CarId, CarName: bodyCar.CarName, DayRate: bodyCar.DayRate, MonthRate: bodyCar.MonthRate, Image: bodyCar.Image}
+	car := models.Car{CarName: bodyCar.CarName, DayRate: bodyCar.DayRate, MonthRate: bodyCar.MonthRate, Image: bodyCar.Image}
 
 	result := initializers.DB.Create(&car)
 
 	if result.Error != nil {
-		c.Status(400)
+		// Return error
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":    http.StatusInternalServerError,
+			"message": "Internal Server Error",
+		})
 		return
 	}
 
-	// Return
-	c.JSON(201, gin.H{
-		"code":    201,
+	// Return Response
+	c.JSON(http.StatusCreated, gin.H{
+		"code":    http.StatusCreated,
 		"message": "OK",
-		"data":    car,
+	})
+}
+
+func GetCarV1(c *gin.Context) {
+	//Get the All of Car Data
+	var cars []models.Car
+
+	if err := initializers.DB.Preload("Orders").Find(&cars).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":    http.StatusInternalServerError,
+			"message": "Failed to fetch cars with orders",
+		})
+		return
+	}
+
+	//Return Response
+	c.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"data": cars,
+	})
+}
+
+func GetCarV1ById(c *gin.Context) {
+	//Get Param
+	id := c.Param("id")
+
+	//Get the All of Car Data
+	var cars models.Car
+	result := initializers.DB.First(&cars, id)
+
+	// Checking the data
+	if result.Error != nil {
+		// Returning 404
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":    http.StatusInternalServerError,
+			"message": "Internal Server Error",
+		})
+		// Return nothing
+		return
+	}
+
+	//Return Response
+	c.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"data": cars,
+	})
+}
+
+func PutCarV1(c *gin.Context) {
+	//Get Param
+	id := c.Param("id")
+
+	// Get data req body
+	var bodyCar struct {
+		CarName   string  `json:"car_name" binding:"required"`
+		DayRate   float64 `json:"day_rate" binding:"required"`
+		MonthRate float64 `json:"month_rate" binding:"required"`
+		Image     string  `json:"image" binding:"required"`
+	}
+
+	if err := c.Bind(&bodyCar); err != nil {
+		c.JSON(http.StatusServiceUnavailable, gin.H{
+			"code":    http.StatusServiceUnavailable,
+			"message": "Bad Request: Invalid JSON payload",
+		})
+		return
+	}
+
+	// Find the data with ID
+	var car models.Car
+	result := initializers.DB.First(&car, id)
+
+	// Sending 404
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"code":    http.StatusNotFound,
+			"message": "Not Found",
+		})
+		return
+	}
+
+	//Update the data
+	putResult := initializers.DB.Model(&car).Updates(models.Car{
+		CarName:   bodyCar.CarName,
+		DayRate:   bodyCar.DayRate,
+		MonthRate: bodyCar.MonthRate,
+		Image:     bodyCar.Image,
+	})
+
+	if putResult.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":    http.StatusInternalServerError,
+			"message": "Internal Server Error",
+		})
+		return
+	}
+
+	// Return with response
+	c.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"message": "Updated Succesfully",
+	})
+}
+
+func DeleteCarV1(c *gin.Context) {
+	//Get Param
+	id := c.Param("id")
+
+	// Find the data with ID
+	var car models.Car
+	result := initializers.DB.First(&car, id)
+
+	// Checking data
+	if result.Error != nil {
+		// Response with 404
+		c.JSON(http.StatusNotFound, gin.H{
+			"code":    http.StatusNotFound,
+			"message": "Not Found",
+		})
+		return
+	}
+
+	delResult := initializers.DB.Delete(&models.Car{}, id)
+
+	// Checking error
+	if delResult.Error != nil {
+		// Response with 404
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    http.StatusBadRequest,
+			"message": "Bad Request",
+		})
+		return
+	}
+
+	// Return Response
+	c.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"message": "Delete Succesfully",
 	})
 }
--- main.go
+++ main.go
@@ -14,7 +14,18 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.POST("/rentalcar/v1/post/car", controllers.PostCarV1)
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// Car Controller
+	r.POST("/rentalcar/v1/post/car", controllers.PostCarV1)
+	r.PUT("/rentalcar/v1/put/car/:id", controllers.PutCarV1)
+	r.DELETE("/rentalcar/v1/delete/car/:id", controllers.DeleteCarV1)
+	r.GET("/rentalcar/v1/get/car", controllers.GetCarV1)
+	r.GET("/rentalcar/v1/get/car/:id", controllers.GetCarV1ById)
+
+	// Order Controller
+	r.POST("/rentalcar/v1/post/order", controllers.PostOrderV1)
+	r.GET("/rentalcar/v1/get/order", controllers.GetOrderV1)
+	r.GET("/rentalcar/v1/get/order/:id", controllers.GetOrderV1ById)
+
+	r.Run() // listen and serve
 }
